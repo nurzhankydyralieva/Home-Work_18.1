2b5e80821456901f2718632eb98af5676c8527fc
nurzhan kydyralieva <nurzhankydyralieva@gmail.com>
2021-01-27 11:07:57 +0600

    added Linus Tovalds quote

--- src/Developer.java
+++ src/Developer.java
@@ -13,8 +13,10 @@ public class Developer extends Employee{
     }
     public void print(){
         super.print();
-
         System.out.println("Developer has grades  " + this.getGrades());
+    }
+    public void quotesTorvalds(){
+        System.out.println("Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.");
     }
     public void writeCode(){
         System.out.println("We are working on great project.");
